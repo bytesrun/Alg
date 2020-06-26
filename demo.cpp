92707e24e9c25a1812b5762f9d313e379421032e
mptbelton <mptbelton@126.com>
2020-06-25 23:06:59 -0700

    add configure.h.in for CMake System

--- demo.cpp
+++ demo.cpp
@@ -6,21 +6,39 @@
 #include <algorithm>
 
 #include "InitNums.h"
-#include "InsertionSort.h"
-#include "FindMaxSubArray.h"
+
+#include "configure.h"
+
+#ifdef INSERT_SORT
+# include "InsertionSort.h"
+#endif
+
+#ifdef FIND_MAX_SUBARRAY
+# include "FindMaxSubArray.h"
+#endif 
+
+#ifdef MATRIX_MULTIPLY
 #include "test_matrix.h"
+#endif
+
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	std::cout << "HELLO WORLD" << std::endl;
+	std::cout << "HELLO TEST " << std::endl;	
+	
+#ifdef INSERT_SORT
 	test();
-	
-	
 	Test_InsertSort();
 	Test_InsertSort_r();
-	
+#endif
+
+#ifdef FIND_MAX_SUBARRAY
 	Test_FindMaxSubArray();
+#endif
+
+#ifdef MATRIX_MULTIPLY	
 	Test_Matrix();
+#endif
 	
 	return 0;
 }
